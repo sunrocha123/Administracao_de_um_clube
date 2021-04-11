b0515976223f1c686fa91a8b2d847b1ea37e2389
Judson Santana <76632803+sunrocha123@users.noreply.github.com>
2021-04-11 10:37:54 -0300

    Update
    Finalizado o processo para cadastro de usuário. Em paralelo, foi iniciado o processo para atualização cadastral

--- Acoes/Acoes_associado.py
+++ Acoes/Acoes_associado.py
@@ -79,48 +79,7 @@ class Associados(object):
                     conn_DB.execute("INSERT INTO DOCUMENTO VALUES (?,?,?,?,?)",
                                     IDcliente, IDtipoDocumento, numero_documento, dtEmissao, validacao)
 
-
-                    #Trecho abaixo para cadastrado de endereço no banco de dados
-
-                    #Validando se o estado informado já consta no BD para coleta do ID
-                    conn_DB.execute(f"SELECT ID FROM ESTADO WHERE NOME = '{estado}'")
-                    IDestado = conn_DB.fetchval()
-
-                    if IDestado == None:
-                        conn_DB.execute(f"INSERT INTO ESTADO (NOME) VALUES ('{estado}')")
-
-                        #Coletando o ID do estado cadastrado
-                        conn_DB.execute(f"SELECT ID FROM ESTADO WHERE NOME = '{estado}'")
-                        IDestado = conn_DB.fetchval()
-
-                    #Validando se a cidade informado já consta no BD para coleta do ID
-                    conn_DB.execute(f"SELECT ID FROM CIDADE WHERE NOME = '{cidade}'")
-                    IDcidade = conn_DB.fetchval()
-
-                    if IDcidade == None:
-                        conn_DB.execute(f"INSERT INTO CIDADE VALUES (?, ?)", IDestado, cidade)
-
-                        #Coletando o ID da cidade cadastrado
-                        conn_DB.execute(f"SELECT ID FROM CIDADE WHERE NOME = '{cidade}'")
-                        IDcidade = conn_DB.fetchval()
-
-                    #Validando se o tipo de endereço informado já consta no BD para coleta do ID
-                    conn_DB.execute(f"SELECT ID FROM TIPO_ENDERECO WHERE NOME = '{tipo_endereco}'") 
-                    IDtipoendereco = conn_DB.fetchval()
-
-                    if IDtipoendereco == None:
-                        conn_DB.execute(f"INSERT INTO TIPO_ENDERECO (NOME) VALUES ('{tipo_endereco}')")
-
-                        #Coletando o ID do tipo de endereço cadastrado
-                        conn_DB.execute(f"SELECT ID FROM TIPO_ENDERECO WHERE NOME = '{tipo_endereco}'") 
-                        IDtipoendereco = conn_DB.fetchval()
-
-                    conn_DB.execute("INSERT INTO ENDERECO VALUES (?,?,?,?,?,?,?,?,?)",
-                                    IDcliente, IDtipoendereco, IDcidade,
-                                    tipo_logradouro, nome_logradouro, complemento,
-                                    numero, bairro, cep)
-
-
+                    self.cont_cadastrar_novo_associado(conn_DB, estado, cidade, tipo_endereco, IDcliente, tipo_logradouro, nome_logradouro, complemento, numero, bairro, cep)
                     conn_DB.commit()
                     print(f"{datetime.now().strftime('%H:%M:%S')}: Usuário cadastrado com sucesso!\n")
                 except Exception as error:
@@ -132,8 +91,67 @@ class Associados(object):
 
         pass
 
+    def cont_cadastrar_novo_associado(self, conn_DB, estado, cidade, tipoEndereco, IDCliente, tipoLogradouro, nomeLogradouro, complemento, numero, bairro, cep):
+        #Trecho abaixo para cadastrado de endereço no banco de dados
+
+        #Validando se o estado informado já consta no BD para coleta do ID
+        conn_DB.execute(f"SELECT ID FROM ESTADO WHERE NOME = '{estado}'")
+        IDestado = conn_DB.fetchval()
+
+        if IDestado == None:
+            conn_DB.execute(f"INSERT INTO ESTADO (NOME) VALUES ('{estado}')")
+
+            #Coletando o ID do estado cadastrado
+            conn_DB.execute(f"SELECT ID FROM ESTADO WHERE NOME = '{estado}'")
+            IDestado = conn_DB.fetchval()
+
+        #Validando se a cidade informado já consta no BD para coleta do ID
+        conn_DB.execute(f"SELECT ID FROM CIDADE WHERE NOME = '{cidade}'")
+        IDcidade = conn_DB.fetchval()
+
+        if IDcidade == None:
+            conn_DB.execute(f"INSERT INTO CIDADE VALUES (?, ?)", IDestado, cidade)
+
+            #Coletando o ID da cidade cadastrado
+            conn_DB.execute(f"SELECT ID FROM CIDADE WHERE NOME = '{cidade}'")
+            IDcidade = conn_DB.fetchval()
+
+        #Validando se o tipo de endereço informado já consta no BD para coleta do ID
+        conn_DB.execute(f"SELECT ID FROM TIPO_ENDERECO WHERE NOME = '{tipoEndereco}'") 
+        IDtipoendereco = conn_DB.fetchval()
+
+        if IDtipoendereco == None:
+            conn_DB.execute(f"INSERT INTO TIPO_ENDERECO (NOME) VALUES ('{tipoEndereco}')")
+
+            #Coletando o ID do tipo de endereço cadastrado
+            conn_DB.execute(f"SELECT ID FROM TIPO_ENDERECO WHERE NOME = '{tipoEndereco}'") 
+            IDtipoendereco = conn_DB.fetchval()
+
+        conn_DB.execute("INSERT INTO ENDERECO VALUES (?,?,?,?,?,?,?,?,?)",
+                        IDCliente, IDtipoendereco, IDcidade,
+                        tipoLogradouro, nomeLogradouro, complemento,
+                        numero, bairro, cep)
+        pass
+
     def alterar_associado(self):
-        # Pendente criação
+        # Necessário validar se o nº do documento do usuário está cadastrado no DB
+        print('Áreas de alteração\n1. Dados pessoais\n2. Endereço\n\n')
+        while True:
+            try:
+                opcao = int(input('Digite a opção desejada (1-2): '))
+                if opcao == 1:
+                    print('Campos Dados Pessoais\n')
+                    camposDadosPessoais = ['nome', 'sobrenome', 'tipo_documento', 'numero_documento', 'dtEmissao', 'validade', 'n_dependentes']
+                    for i in range(len(camposDadosPessoais)):
+                        print((i + 1) + ". " + camposDadosPessoais[i])
+                    break
+                elif opcao == 2:
+                    # Alteração de campos do endereço
+                    break
+                else:
+                    print("Opção inválida! Digite novamente.....")    
+            except ValueError:
+                print('Opção inválida! Digite novamente.....')         
         pass
 
     def excluir_associado(self):
